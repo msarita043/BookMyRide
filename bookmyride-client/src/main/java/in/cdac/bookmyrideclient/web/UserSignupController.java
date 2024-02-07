2243a350807a1a85599e29c403f8d833fe110ce4
msarita043 <msarita043@gmail.com>
2024-02-07 23:39:15 +0530

    added signup form submission and validation

--- bookmyride-client/src/main/java/in/cdac/bookmyrideclient/web/UserSignupController.java
+++ bookmyride-client/src/main/java/in/cdac/bookmyrideclient/web/UserSignupController.java
@@ -1,22 +1,33 @@
 package in.cdac.bookmyrideclient.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import in.cdac.bookmyrideclient.model.Users;
+import in.cdac.bookmyrideclient.model.UserSignUpForm;
+import jakarta.validation.Valid;
 
 @Controller
 public class UserSignupController {
-	
+
 	@GetMapping("/user-signup")
-	public String userSignup() {
+	public String userSignup(@ModelAttribute("userSignupForm") UserSignUpForm userSignupForm, Model model) {
 		return "userSignUp";
 	}
-	
+
 	@PostMapping("/user-signup")
-	public String userSignup(@ModelAttribute("users") Users users ) {
-		System.out.println(users);
-		return "redirect:/user-signup";
+	public String userSignup(@Valid @ModelAttribute("userSignupForm") UserSignUpForm userSignupForm,
+			BindingResult validationErrorResult, Model model) {
+		if (userSignupForm.getConfirmPassword() == null || userSignupForm.getConfirmPassword().isEmpty()
+				|| !(userSignupForm.getPassword().equals(userSignupForm.getConfirmPassword()))) {
+			validationErrorResult.rejectValue("confirmPassword", "error.userSignupForm", "Should be same as password.");
+		}
+		if (validationErrorResult.hasErrors()) {
+			return "userSignUp";
+		} else {
+			return "redirect:/user-signup";
+		}
 	}
 }
