c33a40bf34a17c0a5e9139ea8164b3bf4a37cdde
msarita043 <msarita043@gmail.com>
2024-02-13 17:54:04 +0530

    add new driver

--- bookmyride-client/src/main/java/in/cdac/bookmyrideclient/model/BookNewRideForm.java
+++ bookmyride-client/src/main/java/in/cdac/bookmyrideclient/model/BookNewRideForm.java
@@ -2,14 +2,16 @@ package in.cdac.bookmyrideclient.model;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class BookNewRideForm {
-	
 	private Integer carTypeId;
 	private Integer travelhours;
 	private Integer charges;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
 	private Date pickuptime;
 	private String pickupaddress;
 	private String pickuplocation;
--- bookmyride-client/src/main/java/in/cdac/bookmyrideclient/service/CarTypeService.java
+++ bookmyride-client/src/main/java/in/cdac/bookmyrideclient/service/CarTypeService.java
@@ -1,6 +1,8 @@
 package in.cdac.bookmyrideclient.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,12 @@ public class CarTypeService {
 	public List<CarType> getAllCarTypes() {
 		return webClient.get().uri("/carType/getAllCarTypes").retrieve()
 				.bodyToMono(new ParameterizedTypeReference<List<CarType>>() {
+				}).block();
+	}
+
+	public Optional<CarType> getCarTypeById(int carTypeId) {
+		return webClient.post().uri("/carType/getCarTypeById/"+carTypeId).retrieve()
+				.bodyToMono(new ParameterizedTypeReference<Optional<CarType>>() {
 				}).block();
 	}
 
