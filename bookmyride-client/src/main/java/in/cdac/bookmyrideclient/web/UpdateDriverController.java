e3ac3bb0b3ddfadde52c2ad3036f19ff8c0e38f0
msarita043 <msarita043@gmail.com>
2024-02-15 09:47:33 +0530

    updates in updateDriver

--- bookmyride-client/src/main/java/in/cdac/bookmyrideclient/web/UpdateDriverController.java
+++ bookmyride-client/src/main/java/in/cdac/bookmyrideclient/web/UpdateDriverController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-
 import in.cdac.bookmyrideclient.enums.Roles;
 import in.cdac.bookmyrideclient.model.CarType;
 import in.cdac.bookmyrideclient.model.Driver;
@@ -28,10 +26,10 @@ public class UpdateDriverController {
 
 	@Autowired
 	CarTypeService carTypeService;
-	
+
 	@Autowired
 	DriverService driverService;
-	
+
 	@GetMapping("/update-driver/{driverId}")
 	public String updateDriver(@ModelAttribute("updateDriverForm") UpdateDriverForm updateDriverForm, Model model,
 			HttpSession httpSession, @PathVariable Integer driverId) {
@@ -49,22 +47,21 @@ public class UpdateDriverController {
 		model.addAttribute("driver", driver.get());
 		return "updateDriver";
 	}
-	
+
 	@PostMapping("/update-driver/{driverId}")
-	public String updateDriverPost(@Valid @ModelAttribute("updateDriverForm") UpdateDriverForm updateDriverForm, Users u, BindingResult bindingResult, Model model,
-			HttpSession httpSession, @PathVariable Integer driverId) {
+	public String updateDriverPost(@Valid @ModelAttribute("updateDriverForm") UpdateDriverForm updateDriverForm, BindingResult bindingResult, Model model, HttpSession httpSession,
+			@PathVariable Integer driverId) {
 		Optional<Driver> driver = driverService.getDriver(driverId);
 		List<CarType> carTypeList = carTypeService.getAllCarTypes();
 		model.addAttribute("cartypes", carTypeList);
 		model.addAttribute("driver", driver.get());
-		
-		if(bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			return "updateDriver";
 		}
+		Users u = new Users();
+		u.setName(updateDriverForm.getName());
 		Driver d = driverService.getDriver(updateDriverForm, u);
-		Driver updatedDriver = driverService.updateDriver(d, driverId);
-		
-		
+		driverService.updateDriver(d, driverId);
 		return "redirect:/admin-dashboard";
 	}
 }
